1b642bf5f3baaebb31e820bdc43650eb8afeafaa
ggsbv <ggsbv@protonmail.com>
2018-01-27 18:28:57 +0000

    first test passing, created second test for converting the number 2 to roman numeral 'II'

---
+++ RomanNumerals.js
@@ -0,0 +1,7 @@
+module.exports = class RomanNumerals {
+
+    convert(number) {
+        return "I";
+    }
+
+}
--- test/RomanNumeralsConverter.test.js
+++ test/RomanNumeralsConverter.test.js
@@ -1,6 +1,15 @@
+let assert = require("assert");
+const RomanNumerals = require("../RomanNumerals");
+
 describe("Roman Numerals Converter", () => {
 
     it("should convert the roman numeral 'I' to the number 1", () => {
+        let romanNumerals = new RomanNumerals();
+
+        assert.equal(romanNumerals.convert(1), "I");
+    })
+
+    it("should convert the roman numeral 'II' to the number 2", () => {
         let romanNumerals = new RomanNumerals();
 
         assert.equal(romanNumerals.convert(1), "I");
