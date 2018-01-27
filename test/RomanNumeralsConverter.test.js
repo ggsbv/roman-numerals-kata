8a69386137abcb3c470102f68de7dbc93ee76574
ggsbv <ggsbv@protonmail.com>
2018-01-27 18:35:52 +0000

    wrote test for converting the number 3 to the roman numeral 'III'

--- test/RomanNumeralsConverter.test.js
+++ test/RomanNumeralsConverter.test.js
@@ -3,16 +3,21 @@ const RomanNumerals = require("../RomanNumerals");
 
 describe("Roman Numerals Converter", () => {
 
-    it("should convert the roman numeral 'I' to the number 1", () => {
+    it("should convert the number 1 to the Roman Numeral 'I'", () => {
         let romanNumerals = new RomanNumerals();
 
         assert.equal(romanNumerals.convert(1), "I");
     })
 
-    it("should convert the roman numeral 'II' to the number 2", () => {
+    it("should convert the number 2 to the Roman Numeral 'II'", () => {
         let romanNumerals = new RomanNumerals();
 
         assert.equal(romanNumerals.convert(2), "II");
     })
 
+    it("should convert the number 3 to the Roman Numeral 'III'", () => {
+        let romanNumerals = new RomanNumerals();
+
+        assert.equal(romanNumerals.convert(3), "III");
+    })
 })
