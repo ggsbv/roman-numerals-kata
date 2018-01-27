eefaa1e1cb2c960feaa80539c7a8b89703dd5a4a
ggsbv <ggsbv@protonmail.com>
2018-01-27 18:10:14 +0000

    fixed test, roman numerals converter should return the string 'I' when given the number 1

--- test/RomanNumeralsConverter.test.js
+++ test/RomanNumeralsConverter.test.js
@@ -3,7 +3,7 @@ describe("Roman Numerals Converter", () => {
     it("should convert the roman numeral 'I' to the number 1", () => {
         let romanNumerals = new RomanNumerals();
 
-        assert.equal(romanNumerals.convert("I"), 1);
+        assert.equal(romanNumerals.convert(1), "I");
     })
 
 })
